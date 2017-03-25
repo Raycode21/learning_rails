0667e337ec322aae80eb6c5a5b40c29486081a1f
raycode21 <wanjiku.rachel21@gmail.com>
2017-03-25 10:59:16 +0300

    25 march

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
 belongs_to :article
-  validates :title,presence: true,
+  validates :commenter,presence: true,
   length:{minimum: 5}
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170318101504) do
+ActiveRecord::Schema.define(version: 20170325074832) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -28,6 +31,13 @@ ActiveRecord::Schema.define(version: 20170318101504) do
     t.datetime "updated_at", null: false
   end
 
-  add_index "comments", ["article_id"], name: "index_comments_on_article_id"
+  add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
